d3c9a6a4978d1ecef897e213370b553fb03d9335
msabbasi <msabbasi@ualberta.ca>
2016-03-04 15:29:51 -0700

    "Moved more files"

---
+++ Project1/application.py
@@ -0,0 +1,8 @@
+
+
+class App:
+	def __init__(self, mode):
+		self.appMode = mode
+
+	def obtainInfo(self):
+
---
+++ Project1/statementBuilder.py
@@ -0,0 +1,5 @@
+
+
+class Builder:
+	def __init__(self, mode):
+		self.builderMode = mode
