b8205cdb19858faa84110f08bd7d950fd023eb36
sumeyye aydin <karamehmetler9@gmail.com>
2016-03-15 01:44:30 -0600

    Added files via upload

--- Project1/application.py
+++ Project1/application.py
@@ -83,6 +83,28 @@ class App:
 		self.comm.insert(people, 'people')
 		print("SIN#" + sin + " successfully registered.")
 
+	def CheckSeller(self, seller_id, vehicle_id):
+		curs = self.comm.connection.cursor()
+		check = "SELECT * FROM owner o WHERE vehicle_id = '" + vehicle_id+" owner_id = '" + seller_id+" is_primary_owner = y'"
+		curs.execute(check)
+		row = curs.fetchall()
+		curs.close()
+		if (len(row)) == 0):
+			return False
+		else:
+			return True
+ 
+	def checkPersonReg(self, sin):
+		curs = self.comm.connection.cursor()
+		check = "SELECT * FROM people p WHERE p.sin = '" + sin + "'"
+		curs.execute(check)
+		row = curs.fetchall()
+		curs.close()
+		if (len(row) == 0):
+			return False
+		else:
+			return True
+	
 	def autoTransaction(self):
 		auto_sale = {}
 
@@ -114,9 +136,16 @@ class App:
 				vehicle_id = input("Vehicle serial #: ")
 		auto_sale['vehicle_id'] = vehicle_id
 		#TODO: Check if exists
-		
+		if not self.CheckSeller(seller_id, vehicle_id): #Need to check primary owner. I added owner dict in vehicle registration.
+			print("The person who tries to sell the vechicle is not primary owner")
+			print("Please, try auto transaction with the primary owner of the vehicle")
+			#del auto_sale['seller_id'] #I added these codes but I'm not sure about them.  
+			#del auto_sale['buyer_id']
+			#del auto_sale['vehicle_id']
+			#autoTransaction()
+
 		saleDate = input("Date of the transaction (dd-mm-yyyy): ")
-		auto_sale['s_date'] = parse(saleDate, dayfirst=True)
+		auto_sale['s_date'] = parse(saleDate, dayfirst=True) 
 
 		auto_sale['price'] = input("Price of the vehicle sold: ")
 
@@ -130,35 +159,35 @@ class App:
 
 		
 	def driverLicenceReg(self):
-		driverLicenceReg = {}
+		driver_licence = {}
 		licence_no = input("Please enter Licence Number:")
 		while( len(licence_no)>15 or license_no == ""): #If licence no has more than 15 character and does not entered anything
 			print("The licence number that you entered is invalid. Please try again.")
 			licence_no = input("Please enter Licence Number:")
-		diverLicenceReg['licence_no'] = licence_no
+		diver_licence['licence_no'] = licence_no
 		sin = input("Please enter Social Insurance Number:")
 		while( len(sin) > 15 or sin == "" ):
 			print("The Social Insurance Number that you entered is invalid. Please try again.")
 			sin = input("Please enter Social Insurance Number:")
 		if not self.checkPersonReg(sin):
 			regPerson(sin)
-		driverLicenceReg['sin'] = sin
+		driver_licence['sin'] = sin
 		licence_class = input("Please enter Licence Class:")
 		while ( len(licence_class)>10 or licence_no == ""):
 			print ("The Licence Class that you entered is invalid. Please try again.")
 			licence_class = input("Please enter Licence Class:")
-		driverLicenceReg['class'] = licence_class 
+		driver_licence['class'] = licence_class 
 		photo_name = input("Please insert photo for licence (Optional) :")
 		issuing_date = ("Please enter issuing date of the licence in MM-DD-YYYY format:")
 		while ( is_date_valid(issuing_date) == False): #I need to check if it is in MM-DD-YYYY format
 			print ("Issuing Date that you entered is invalid. Please try again.")
 			issuing_date = ("Please enter issuing date of the licence in MM-DD-YYYY format:")
-		driverLicenceReg['issuing_date'] = issuing_date
+		driver_licence['issuing_date'] = issuing_date
 		expiring_date = ("Please enter expiring date of the licence in MM-DD-YYYY format:")
 		while ( is_date_valid(expiring_date) == False ): #I need to check if it is in MM-DD-YYYY format
 			print ("Expiring Date that you entered is invalid. Please try again.")
 			expiring_date = ("Please enter expiring date of the licence in MM-DD-YYYY format:")
-		driverLicenceReg['expiring_date'] = expiring_date
+		driver_licence['expiring_date'] = expiring_date
 		print("Wait, we are processing...")
 		try: 
 			if (photo_name == ""):
@@ -166,9 +195,9 @@ class App:
 			else:
 				f_photo = open(photo_name, 'rb')
 				photo = f_photo.read()
-				driverLicenceReg['photo'] = photo
+				driver_licence['photo'] = photo
 			
-			self.comm.insert(driverLicenceReg)
+			self.comm.insert(driver_licence, 'driver_licence')
 			print("You registered the new driver licence!")
 		except cx_Oracle.DatabaseError as exc:
 			error=exc.args
@@ -180,44 +209,58 @@ class App:
 	def vehicleReg(self):
 
 		vehicle = {}
+		owner = {}
+		
+		serial_no =  self.comm.getNewID('vehicle', 'serial_no')
 
-		vehicle['serial_no'] = self.comm.getNewID('vehicle', 'serial_no')
+		vehicle['serial_no'] = serial_no
 		
 		maker = input("Please enter the make of the vehicle:")
 		while( len(maker) > 20 or maker == ""): #if maker is invalid
 			print("The make that you have entered is invalid. Please try again.")
 			maker = input("Please enter the make of the vehicle:")
-		
+		vehicle['maker'] = maker
+
 		model = input("Please enter the model of the vehicle:")
 		while( len(model) > 20 or model == ""): #if model is invalid
 			print("The model that you have entered is invalid. Please try again.")
 			model = input("Please enter the model of the vehicle:")
-		
+		vehicle['model'] = model
+
 		year = int(input("Please enter the year of the vehicle:"))
 		while( year > 9999 or year < 1): #if year is not between 1-4 digits 
 			print("The year that you have entered is invalid. Please try again.")
 			year = int(input("Please enter the year of the vehicle:"))
-		
+		vehicle['year'] = year
+
 		color = input("Please enter the color of the vehicle:")
 		while( len(color) > 10 or color == ""): # if color is invalid
 			print("The color that you have entered is invalid. Please try again.")
 			color = input("Please enter the color of the vehicle:")
-		
+		vehicle['color'] = color
+	
 		type_id = int(input("Please enter the type_id:"))
 		while( type(type_id) != int): # type_id not an integer
 			print("The type_id is invalid. Please try again.")
 			type_id = int(input("Please enter the type_id:"))
+		vehicle['type_id'] = type_id
 		
+		owner['vehicle_id'] = serial_no
 		owner_id = input("Please enter the persons sin number:")
 		while( len(owner_id) > 15 or owner_id == ""): # if sin is invalid
 			print("The sin is invalid. Please try again.")
 			owner_id = input("Please enter the persons sin number:")
-		
+		owner['owner_id'] = owner_id
+
 		prim_own = input("Are they a primary owner('y' or 'n'):")
 		while( prim_own != 'y' or prim_own != 'n'):
 			print("Invalid input. Please try again.")
 			prim_own = input("Are they a primary owner('y' or 'n'):")
-			
+		owner['is_primary_owner'] = prim_own
+
+		self.comm.insert(vehicle, 'vehicle')
+		self.comm.insert(owner, 'owner')
+	
 	def violationRec(self):
 		
 		#TODO: Better error handling
