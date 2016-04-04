74fe82449e9239ab56db875654a9adc940cd1734
msabbasi <msabbasi@ualberta.ca>
2016-04-04 17:43:46 -0600

    added more functions to main.py

--- Project2/main.py
+++ Project2/main.py
@@ -13,7 +13,6 @@ def get_random_char():
     return chr(97 + random.randint(0, 25))
 
 def create_database(mode):
-    database = db.DB()
     try:
         # create a btree file
         if mode == 'btree':
@@ -43,50 +42,68 @@ def create_database(mode):
         value = value.encode(encoding='UTF-8')
         database.put(key, value);
 
+def destroy_database():
+    try:
+        database.close()
+    except Exception as e:
+        print (e)
 
-
-if len(sys.argv) < 2:
-    print("Usage: mydbtest db_type_option where db_type_option can be btree, hash or indexfile")
-    sys.exit()
-else:
-    mode = sys.argv[1]
-    if mode != 'btree' and mode != 'hash' and mode != 'indexfile':
+def main():
+    if len(sys.argv) < 2:
         print("Usage: mydbtest db_type_option where db_type_option can be btree, hash or indexfile")
         sys.exit()
+    else:
+        mode = sys.argv[1]
+        if mode != 'btree' and mode != 'hash' and mode != 'indexfile':
+            print("Usage: mydbtest db_type_option where db_type_option can be btree, hash or indexfile")
+            sys.exit()
 
+    database = db.DB()
 
-while(True):
-	print()
-	print("====================================================================")
-	print("                               MAIN MENU                            ")
-	print("====================================================================")
+    while(True):
+	    print()
+	    print("====================================================================")
+	    print("                               MAIN MENU                            ")
+	    print("====================================================================")
 
-	# Get the user to choose an option
-	try:
-		choice = int(input("""Please select the number corresponding to your choice: \n1 - Create and populate a database \n2 - Retrieve records with a given key \n3 - Retrieve records with a given data \n4 - Retrieve records with a given range of key values \n5 - Destroy the database \n6 - Quit \nType a number: """ ))
-	except ValueError:
-		choice = 0
-	while (choice > 6 or choice < 1):
-		try:
-			choice = int(input("Choice not valid. Please choose a number between 1-6: "))
-		except ValueError:
-			choice = 0
+	    # Get the user to choose an option
+	    try:
+		    choice = int(input("""Please select the number corresponding to your choice: \n1 - Create and populate a database \n2 - Retrieve records with a given key \n3 - Retrieve records with a given data \n4 - Retrieve records with a given range of key values \n5 - Destroy the database \n6 - Quit \nType a number: """ ))
+	    except ValueError:
+		    choice = 0
+	    while (choice > 6 or choice < 1):
+		    try:
+			    choice = int(input("Choice not valid. Please choose a number between 1-6: "))
+		    except ValueError:
+			    choice = 0
 
-	# Exit if exit option choosen
-	if (choice == 6):
-		print("Exiting...")
-		break
+	    # Exit if exit option choosen
+	    if choice == 6:
+		    print("Exiting...")
+		    break
+        elif choice == 1:
+            create_database(mode)
+        elif choice == 2:
+    	    print(choice)
+        elif choice == 3:
+    	    print(choice)
+        elif choice == 4:
+    	    print(choice)
+        elif choice == 5:
+    	    destroy_database()
 
+	    # Start the app supplying the choice and the communicator
+	    #app = App(choice, communication)
+	    #result = app.run()
 
-	print(mode)
-	# Start the app supplying the choice and the communicator
-	#app = App(choice, communication)
-	#result = app.run()
-
-	# Exit if the user choose to quit
-	#if result == 0:
-	#	break
+	    # Exit if the user choose to quit
+	    #if result == 0:
+	    #	break
 	
 
-# Clean up
+    # Clean up
+
+
+if __name__ == "__main__":
+    main()
 
