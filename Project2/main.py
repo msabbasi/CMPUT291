7786665fb523109ef33b9f4c9fec8ae369531d9f
msabbasi <msabbasi@ualberta.ca>
2016-04-04 19:27:12 -0600

    some changes in main

--- Project2/main.py
+++ Project2/main.py
@@ -61,45 +61,45 @@ def main():
     database = db.DB()
 
     while(True):
-	    print()
-	    print("====================================================================")
-	    print("                               MAIN MENU                            ")
-	    print("====================================================================")
+        print()
+        print("====================================================================")
+        print("                               MAIN MENU                            ")
+        print("====================================================================")
 
-	    # Get the user to choose an option
-	    try:
-		    choice = int(input("""Please select the number corresponding to your choice: \n1 - Create and populate a database \n2 - Retrieve records with a given key \n3 - Retrieve records with a given data \n4 - Retrieve records with a given range of key values \n5 - Destroy the database \n6 - Quit \nType a number: """ ))
-	    except ValueError:
-		    choice = 0
-	    while (choice > 6 or choice < 1):
-		    try:
-			    choice = int(input("Choice not valid. Please choose a number between 1-6: "))
-		    except ValueError:
-			    choice = 0
+        # Get the user to choose an option
+        try:
+            choice = int(input("""Please select the number corresponding to your choice: \n1 - Create and populate a database \n2 - Retrieve records with a given key \n3 - Retrieve records with a given data \n4 - Retrieve records with a given range of key values \n5 - Destroy the database \n6 - Quit \nType a number: """ ))
+        except ValueError:
+            choice = 0
+        while (choice > 6 or choice < 1):
+            try:
+                choice = int(input("Choice not valid. Please choose a number between 1-6: "))
+            except ValueError:
+                choice = 0
 
-	    # Exit if exit option choosen
-	    if choice == 6:
-		    print("Exiting...")
-		    break
+        # Exit if exit option choosen
+        if choice == 6:
+            print("Exiting...")
+            break
         elif choice == 1:
             create_database(mode)
         elif choice == 2:
-    	    print(choice)
+            print(choice)
         elif choice == 3:
-    	    print(choice)
+            print(choice)
         elif choice == 4:
-    	    print(choice)
+            print(choice)
         elif choice == 5:
-    	    destroy_database()
+            destroy_database()
 
-	    # Start the app supplying the choice and the communicator
-	    #app = App(choice, communication)
-	    #result = app.run()
+        # Start the app supplying the choice and the communicator
+        #app = App(choice, communication)
+        #result = app.run()
 
-	    # Exit if the user choose to quit
-	    #if result == 0:
-	    #	break
-	
+        # Exit if the user choose to quit
+        #if result == 0:
+        #    break
+    
 
     # Clean up
 
